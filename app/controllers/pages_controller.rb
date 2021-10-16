9955c843de17259678b73cf6f7d765a8bc7c4e7d
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-10-16 18:24:34 -0300

    Add validation to movie model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,22 @@ require 'rails/application_controller'
 
 class PagesController < Rails::ApplicationController
   def index
+
     render file: 'app/views/pages/index.html'
+    # @movies = Movie.all
+    # respond_to do |format|
+    #   format.csv {send_data @movies.to_csv}
+    # end
+  end
+
+  def import
+   Movie.import(params[:file])
+   redirect_to root_url, notice: "Movies imported."
+  end
+
+  private
+
+  def movies_doc_params
+    params.require(:movie).permit(:document)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ApplicationRecord
+  # has_one_attached :document
+  validates :title, :description, uniqueness: true
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      Movie.create! row.to_hash
+    end
+  end
+
 end

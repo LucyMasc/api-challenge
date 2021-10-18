8c380cc495dd7267c7bb00d026103f3e6fc34ed5
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-10-17 21:58:48 -0300

    Filters added

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -5,7 +5,7 @@ module Api
         if params[:query].present?
           sql_query = "genre ILIKE :query OR year ILIKE :query OR country ILIKE :query"
           @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
-          render json: { status: 'SUCCESS', message: 'Loaded movies', data: @movies}, status: :ok
+          render json: { status: 'SUCCESS', message: "Loaded movies by #{params[:query]}", data: @movies}, status: :ok
         else
           # @movies = Movie.all
           @movies = Movie.order('published_at DESC')

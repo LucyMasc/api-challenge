5144c7d7aaae94b3bb94a401b0f121d0f335c817
Luciana Mascarenhas <lucianamascarenhas011@hotmail.com>
2021-10-22 09:21:16 -0300

    Change controller rspec endpoint name, also movie_type to genre on controller query.

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -3,7 +3,7 @@ module Api
     class MoviesController < ApplicationController
       def index
         if params[:query].present?
-          sql_query = "movie_type ILIKE :query OR release_year ILIKE :query OR country ILIKE :query"
+          sql_query = "genre ILIKE :query OR release_year ILIKE :query OR country ILIKE :query"
           @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
           render json: { status: 'SUCCESS', message: "Loaded movies by #{params[:query]}", data: @movies}, status: :ok
         else
--- spec/controllers/api/v1/movies_spec.rb
+++ spec/controllers/api/v1/movies_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::MoviesController, type: :request do
-  describe 'GET /api/v1/movies?query=' do
+  describe 'GET /movies?query=' do
     before(:each) do
-      Movie.create!(title: 'Titanic', genre: "Movie", year: 2018, country: 'United States', published_at: '2020-7-5' , description: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic, 84 years later.')
-      Movie.create!(title: 'Wonder Woman', genre: "Tv Show", year: 2020, country: 'United States', published_at: '2018-7-5' , description: 'When a pilot crashes and tells of conflict in the outside world, Diana, an Amazonian warrior in training, leaves home to fight a war, discovering her full powers and true destiny.')
+      Movie.create!(title: 'Titanic', genre: "Movie", release_year: 2018, country: 'United States', published_at: '2020-7-5' , description: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic, 84 years later.')
+      Movie.create!(title: 'Wonder Woman', genre: "Tv Show", release_year: 2020, country: 'United States', published_at: '2018-7-5' , description: 'When a pilot crashes and tells of conflict in the outside world, Diana, an Amazonian warrior in training, leaves home to fight a war, discovering her full powers and true destiny.')
     end
 
     let(:genre) { 'Movie' }
@@ -12,25 +12,25 @@ RSpec.describe Api::V1::MoviesController, type: :request do
     let(:country) { 'United States' }
 
     it 'Should return all movies' do
-      get '/api/v1/movies'
+      get '/movies'
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body)["data"].count).to eq(2)
     end
 
     it 'should return movies with same genre' do
-      get "/api/v1/movies?query=#{genre}"
+      get "/movies?query=#{genre}"
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body)["data"].count).to eq(1)
     end
 
     it 'should return movies with same year' do
-      get "/api/v1/movies?query=#{year}"
+      get "/movies?query=#{year}"
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body)["data"].count).to eq(1)
     end
 
     it 'should return movies from same country' do
-      get "/api/v1/movies?query=#{country}"
+      get "/movies?query=#{country}"
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body)["data"].count).to eq(2)
     end
